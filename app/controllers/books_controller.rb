a05ee91d60b29f7ae2ceef61efc7ff7b24a45ce2
kurosu66 <kurosu.66@gmail.com>
2022-12-17 22:47:03 +0900

    不要なrespond_toを削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,28 +26,20 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
 
-    respond_to do |format|
-      if @book.save
-        format.html { redirect_to @book, notice: t('Book was successfully created.') }
-        format.json { render :show, status: :created, location: @book }
-      else
-        format.html { render :new }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
+    if @book.save
+      redirect_to @book, notice: t('Book was successfully created.')
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /books/1
   # PATCH/PUT /books/1.json
   def update
-    respond_to do |format|
-      if @book.update(book_params)
-        format.html { redirect_to @book, notice: t('Book was successfully updated.') }
-        format.json { render :show, status: :ok, location: @book }
-      else
-        format.html { render :edit }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
+    if @book.update(book_params)
+      redirect_to @book, notice: t('Book was successfully updated.')
+    else
+      render :edit
     end
   end
 
@@ -55,10 +47,7 @@ class BooksController < ApplicationController
   # DELETE /books/1.json
   def destroy
     @book.destroy
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: t('Book was successfully destroyed.') }
-      format.json { head :no_content }
-    end
+    redirect_to books_url, notice: t('Book was successfully destroyed.')
   end
 
   private
