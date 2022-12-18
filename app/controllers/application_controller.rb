00b131fd45530ac2e4ff71b5128d542c7bb4f821
kurosu66 <kurosu.66@gmail.com>
2022-12-18 17:12:29 +0900

    使用しないメソッド（URLクエリパラメータでlocaleを切り替える）を削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  around_action :switch_locale
-
-  def switch_locale(&action)
-    locale = params[:locale] || I18n.default_locale
-    I18n.with_locale(locale, &action)
-  end
 end
