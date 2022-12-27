615993bbe453539bcba057bd1675cf2f9ea017cd
kurosu66 <kurosu.66@gmail.com>
2022-12-27 11:07:48 +0900

    Revert "不要なrespond_toを削除"
    This reverts commit a05ee91d60b29f7ae2ceef61efc7ff7b24a45ce2.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,20 +26,28 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
 
-    if @book.save
-      redirect_to @book, notice: t('Book was successfully created.')
-    else
-      render :new
+    respond_to do |format|
+      if @book.save
+        format.html { redirect_to @book, notice: t('Book was successfully created.') }
+        format.json { render :show, status: :created, location: @book }
+      else
+        format.html { render :new }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   # PATCH/PUT /books/1
   # PATCH/PUT /books/1.json
   def update
-    if @book.update(book_params)
-      redirect_to @book, notice: t('Book was successfully updated.')
-    else
-      render :edit
+    respond_to do |format|
+      if @book.update(book_params)
+        format.html { redirect_to @book, notice: t('Book was successfully updated.') }
+        format.json { render :show, status: :ok, location: @book }
+      else
+        format.html { render :edit }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
     end
   end
 
@@ -47,7 +55,10 @@ class BooksController < ApplicationController
   # DELETE /books/1.json
   def destroy
     @book.destroy
-    redirect_to books_url, notice: t('Book was successfully destroyed.')
+    respond_to do |format|
+      format.html { redirect_to books_url, notice: t('Book was successfully destroyed.') }
+      format.json { head :no_content }
+    end
   end
 
   private
