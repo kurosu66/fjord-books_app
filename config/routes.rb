2e60d68aa10d7e4f7adfcf0609b9caacac07e2e8
kurosu <kurosu.66@gmail.com>
2023-02-14 19:54:44 +0900

    不要ファイル削除、routeの不要記述削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'users/sessions',
     registrations: 'users/registrations',
-    passwords: 'users/passwords'
   }
   resources :books
   resources :users, only: %i(index show edit update)
