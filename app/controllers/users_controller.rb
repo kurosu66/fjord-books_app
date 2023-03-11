6ed4b3d2dd1fba1654d06d30315cfc473b6fbe93
kurosu <kurosu.66@gmail.com>
2023-03-11 09:44:36 +0900

    不要な記述を削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,13 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update]
-  before_action :ensure_current_user, only: %i[edit update]
+  before_action :set_user, only: %i[show]
 
   def index
     @users = User.page(params[:page]).order(id: :asc)
   end
 
-  def update
-    User.find(params[:id]).update(user_params)
-    redirect_to @user, notice: t('controllers.common.notice_update', name: User.model_name.human)
-  end
+  def show; end
 
   private
 
@@ -21,10 +17,5 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :postal_code, :address, :introduction)
-  end
-
-  def ensure_current_user
-    user = set_user
-    redirect_to(users_path) unless user == current_user
   end
 end
